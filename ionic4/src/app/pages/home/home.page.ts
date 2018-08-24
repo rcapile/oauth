c69f842cb06fca08a4f772d8f9be25bb211a694f
Rodrigo Capilé <rcapile@gmail.com>
2018-08-24 16:04:07 -0300

    revisão dos tabs

--- ionic4/src/app/pages/home/home.page.ts
+++ ionic4/src/app/pages/home/home.page.ts
@@ -1,55 +1,10 @@
 import {Component} from '@angular/core';
-import {App, LoadingController, NavController, ToastController} from '@ionic/angular';
-import {AuthService} from '../../providers/auth-service/auth-service';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-    loading: any;
-    isLoggedIn = false;
-
-    constructor(public app: App,
-                public navCtrl: NavController,
-                public authService: AuthService,
-                public loadingCtrl: LoadingController,
-                private toastCtrl: ToastController) {
-        if (localStorage.getItem('token')) {
-            this.isLoggedIn = true;
-        }
-    }
-
-    logout() {
-        this.showLoader().then(() => {
-            this.authService.logout().then((result) => {
-                this.loading.dismiss();
-                localStorage.clear();
-                this.navCtrl.goRoot('login');
-            }, (err) => {
-                this.loading.dismiss();
-                this.presentToast(err);
-            });
-        });
-    }
-
-    async showLoader() {
-        this.loading = await this.loadingCtrl.create({
-            content: 'Authenticating...'
-        });
-
-        await this.loading.present();
-    }
-
-    presentToast(msg) {
-        this.toastCtrl.create({
-            message: msg,
-            duration: 3000,
-            position: 'bottom'
-        }).then(toast => toast.present());
-        /* toast.onDidDismiss(() => {
-             console.log('Dismissed toast');
-         });*/
-    }
 }
--- ionic4/src/app/pages/tabs/tabs.page.ts
+++ ionic4/src/app/pages/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {App, LoadingController, NavController, ToastController} from '@ionic/angular';
+import {AuthService} from '../../providers/auth-service/auth-service';
 
 @Component({
     selector: 'app-tabs',
@@ -8,10 +9,55 @@ import {NavController} from '@ionic/angular';
 })
 export class TabsPage {
 
-    constructor(public navCtrl: NavController) {
+  /*  constructor(public navCtrl: NavController) {
         if (!localStorage.getItem('token')) {
             navCtrl.goRoot('login');
         }
+    }
+*/
+    loading: any;
+    isLoggedIn = false;
+
+    constructor(public app: App,
+                public navCtrl: NavController,
+                public authService: AuthService,
+                public loadingCtrl: LoadingController,
+                private toastCtrl: ToastController) {
+        if (localStorage.getItem('token')) {
+            this.isLoggedIn = true;
+        }
+    }
+
+    logout() {
+        this.showLoader().then(() => {
+            this.authService.logout().then((result) => {
+                this.loading.dismiss();
+                localStorage.clear();
+                this.navCtrl.goRoot('login');
+            }, (err) => {
+                this.loading.dismiss();
+                this.presentToast(err);
+            });
+        });
+    }
+
+    async showLoader() {
+        this.loading = await this.loadingCtrl.create({
+            content: 'Authenticating...'
+        });
+
+        await this.loading.present();
+    }
+
+    presentToast(msg) {
+        this.toastCtrl.create({
+            message: msg,
+            duration: 3000,
+            position: 'bottom'
+        }).then(toast => toast.present());
+        /* toast.onDidDismiss(() => {
+             console.log('Dismissed toast');
+         });*/
     }
 
 }
